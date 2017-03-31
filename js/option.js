56e864ba482f4ab41dd6a3a60dd852e3fbc98967
Alex Matheson <alex@kobecreations.com>
2017-03-31 11:53:33 +1100

    Made it work on gh-pages

--- js/option.js
+++ js/option.js
@@ -1,5 +1,5 @@
 var OPT = {
-  dev: true,
+  dev: false,
   dev_port: ':8080',
   sketch_container: '#sketch',
   host: window.location.protocol + '//' + window.location.hostname,
@@ -12,3 +12,4 @@ var OPT = {
 }
 
 if (true) OPT.host = OPT.host + ':8080'
+else OPT.host = OPT.host + '/p5js'
